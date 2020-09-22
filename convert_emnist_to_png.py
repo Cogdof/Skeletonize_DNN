5cc4f4a9888f52b90583844ef0ce9867e2484ecb
Cogdof <chy4748@naver.com>
2020-09-22 18:04:24 +0900

    Add Emnist converter, adate vgg19 to fit emnist data

---
+++ convert_emnist_to_png.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import warnings
+import cv2
+import os
+import gc
+from PIL import Image
+
+'''
+# Convert EMNIST, MNIST, etc binary csv data to jpg image type.
+
+# Dataset from 
+
+# Train :  697931
+# Test :  116322
+
+# Structre
+-----------------------------
+Train | Test
+    /1 / {num}_{label}.jpg
+    /2 
+    ...
+    /A
+    ...
+    /a
+-----------------------------    
+'''
+
+
+
+# mapping label to alphabet for digit
+def mapping(a):
+    if a <= 9:
+        return a + 48
+    elif 10 <= a and a <= 35:
+        return a + 55
+    elif 36 <= a :
+        return a + 61
+
+
+
+warnings.filterwarnings("ignore")
+
+#train = pd.read_csv('/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/test.csv') # test csv case
+train = pd.read_csv('/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/emnist-byclass-train.csv')
+test = pd.read_csv('/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/emnist-byclass-test.csv')
+
+print(train.shape)
+print(test.shape)
+
+train_dir =  '/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/Train/'
+test_dir = '/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/Test/'
+
+i = 0
+for x in train._get_values:
+
+
+    label = chr(mapping(x[0]))
+    vector = x[1:]
+    #print(label)
+    #print(vector)
+    img = vector.reshape(28, 28).astype(int)
+    path = train_dir +"/"+ label
+    if not (os.path.isdir(path)):  # 새  파일들을 저장할 디렉토리를 생성
+        os.makedirs(os.path.join(path))
+
+    # transform to fit rotate, flip.
+
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    img = cv2.flip(img, 1)
+    cv2.imwrite(path+"/{}_{}.jpg".format(i,label) , img)
+
+    #print(i)
+    i= i+1
+
+    gc.collect()
+train_case = i
+gc.collect()
+i = 0
+
+for x in test._get_values:
+
+
+    label = chr(mapping(x[0]))
+    vector = x[1:]
+    img = vector.reshape(28, 28).astype(int)
+    path = train_dir +"/"+ label
+    if not (os.path.isdir(path)):  # 새  파일들을 저장할 디렉토리를 생성
+        os.makedirs(os.path.join(path))
+    cv2.imwrite(path+"/{}_{}.jpg".format(i,label) , img)
+    i = i+1
+    gc.collect()
+test_case = i
+
+# create folder
+
+# save
+
+# gc
+gc.collect()
+print("{} | {} case.".format(train_case, test_case))
+print("Done!")
--- vgg_EMNIST.py
+++ vgg_EMNIST.py
@@ -19,7 +19,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import sys
-
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
 '''
 ==================================================
 [Based network]
@@ -58,7 +59,7 @@ ver 2.2 batch 8 epoch 10, skeletonize(external data)
 ver 2.3 batch 4, epoch 10, skeletonize(external data)
 ver 2.4 batch 4, epoch 5, skeletonize(external data).
     change f1,f2,f3 layers
-    
+
 ver b3.0 batch 4. epoch 5, dataset6 (label : 52 a~z, A~Z, non numberic)
 
 ver 4.0 VGG 26+26+10 case,digit distinguish re train
@@ -74,20 +75,18 @@ label = 35
 #   ver1 ~ 3 (26+10)
 #   ver4 61 = (26 +26 +10)
 
-model_name = "OCR_vgg_ver"+version+"_ep"+str(epoch_count)+"_batch"+str(batch)
+model_name = "OCR_vgg_ver" + version + "_ep" + str(epoch_count) + "_batch" + str(batch)
 
-#data_dir = '/home/mll/v_mll3/OCR_data/인식_100데이터셋/single_character_Data (사본)/beta_skeletonize'
+# data_dir = '/home/mll/v_mll3/OCR_data/인식_100데이터셋/single_character_Data (사본)/beta_skeletonize'
 # data_dir = '/home/mll/v_mll3/OCR_data/dataset/single_character_dataset/dataset/after_skeletonize'
-data_dir ='/home/mll/v_mll3/OCR_data/dataset/single_character_dataset/dataset/data'
+#data_dir = '/home/mll/v_mll3/OCR_data/dataset/single_character_dataset/dataset/data'
+data_dir = '/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass'
 TRAIN = 'Train'
 VAL = 'Validation'
 TEST = 'Test'
 save_path = "/home/mll/v_mll3/OCR_data/VGG_character/model/"
 
-
-
 print(torch.cuda.is_available())
-
 
 transform = transforms.Compose([
     transforms.Resize(224),
@@ -96,55 +95,69 @@ transform = transforms.Compose([
 ])
 
 
-#---------------------------------------
+'''
+# data_dir = '/home/mll/v_mll3/OCR_data/dataset/single_character_dataset/dataset/after_skeletonize'
+data_dir = '/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/'
+TRAIN = 'Train'
+VAL = 'Validation'
+TEST = 'Test'
+save_path = "/home/mll/v_mll3/OCR_data/VGG_character/model/"
 
+input_size = 28
+batch_size = 64
 
-#--------------------------------------
+train_data_dir = data_dir+"emnist-byclass-train.csv"
+test_data_dir = data_dir+"emnist-byclass-test.csv"
+
+train_data = pd.read_csv(train_data_dir)
+test_data = pd.read_csv(test_data_dir)
+
+print(train_data.shape)
+print(test_data.shape)
+'''
 
 
 class Net(nn.Module):
 
     # vector set of classify.
 
-
     def __init__(self):
         super(Net, self).__init__()
         self.conv = nn.Sequential(
-            #3 224 128
-            nn.Conv2d(3, 64, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(64, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            # 3 224 128
+            nn.Conv2d(3, 64, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 64, 3, padding=1), nn.LeakyReLU(0.2),
             nn.MaxPool2d(2, 2),
-            #64 112 64
-            nn.Conv2d(64, 128, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            # 64 112 64
+            nn.Conv2d(64, 128, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 128, 3, padding=1), nn.LeakyReLU(0.2),
             nn.MaxPool2d(2, 2),
-            #128 56 32
-            nn.Conv2d(128, 256, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            # 128 56 32
+            nn.Conv2d(128, 256, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1), nn.LeakyReLU(0.2),
             nn.MaxPool2d(2, 2),
-            #256 28 16
-            nn.Conv2d(256, 512, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            # 256 28 16
+            nn.Conv2d(256, 512, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1), nn.LeakyReLU(0.2),
             nn.MaxPool2d(2, 2),
-            #512 14 8
-            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
-            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            # 512 14 8
+            nn.Conv2d(512, 512, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1), nn.LeakyReLU(0.2),
             nn.MaxPool2d(2, 2)
         )
-        #512 7 4
+        # 512 7 4
 
         self.avg_pool = nn.AvgPool2d(7)
-        #512 1 1
+        # 512 1 1
 
+        # print(self)
+        # last vector
+        # self.last_vector = self
 
-        #print(self)
-        #last vector
-        #self.last_vector = self
-
-        self.classifier = nn.Linear(512, 35)
+        self.classifier = nn.Linear(512, label)
         """
         self.fc1 = nn.Linear(512*2*2,4096)
         self.fc2 = nn.Linear(4096,4096)
@@ -152,26 +165,23 @@ class Net(nn.Module):
         """
 
     def forward(self, x):
-
-        #print(x.size())
+        # print(x.size())
         features = self.conv(x)
-        #print(features.size())
+        # print(features.size())
         x = self.avg_pool(features)
-        #vector = x
-        #print(avg_pool.size())
+        # vector = x
+        # print(avg_pool.size())
         x = x.view(features.size(0), -1)
-        #print(flatten.size())
+        # print(flatten.size())
         vector = x
         x = self.classifier(x)
         result = x
 
-        #x = self.softmax(x)
-        #result = self.softmax(x)
-        #vector = self.last_vector
+        # x = self.softmax(x)
+        # result = self.softmax(x)
+        # vector = self.last_vector
 
         return x, features, result, vector
-
-
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -182,29 +192,34 @@ param = list(net.parameters())
 print(len(param))
 for i in param:
     print(i.shape)
-#print(param[0].shape)
+
+
+# print(param[0].shape)
 
 
 # functions to show an image
 
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
 
 def model_loader():
     model_folder = '/home/mll/v_mll3/OCR_data/VGG_character/model'
     model_list = os.listdir(model_folder)
     model_list.sort()
     print("---------------Select model ------------")
-    for i in range(0 , len(model_list)):
-        print("{} : {}".format(i,  model_list[i]))
+    for i in range(0, len(model_list)):
+        print("{} : {}".format(i, model_list[i]))
     num = input()
-    model_dir = model_folder+ '/'+ model_list[int(num)]
+    model_dir = model_folder + '/' + model_list[int(num)]
     model_name = model_list[int(num)]
-    #print(model_dir)
+    # print(model_dir)
     return model_dir, model_name
+
+
 # VGG-16 Takes 224x2
 
 
@@ -212,9 +227,6 @@ def train():
     criterion = nn.CrossEntropyLoss().cuda()
     optimizer = optim.Adam(net.parameters(), lr=0.00001)  # origin  lr =0.00001
     train_size = dataset_sizes[TRAIN]
-
-
-
 
     for epoch in range(epoch_count):  # loop over the dataset multiple times   #100 epoch -> 3
         running_loss = 0.0
@@ -229,16 +241,16 @@ def train():
             # print(inputs.shape)
             # forward + backward + optimize
 
-            #outputs, f = net(inputs)               # *original
+            # outputs, f = net(inputs)               # *original
 
             outputs, f, result, vector = net(inputs)
 
-            #predicted = torch.max(outputs, 1)
+            # predicted = torch.max(outputs, 1)
 
-            #print(labels)
-            #print(predicted)
-            #print(result)
-            #print(vector)
+            # print(labels)
+            # print(predicted)
+            # print(result)
+            # print(vector)
             # print(outputs.shape)
             # print(labels.shape)1
 
@@ -256,15 +268,15 @@ def train():
             if i % train_size / 1000 == train_size / (1000 - 1):  # print every 2000 mini-batches
                 print('===== [%d, %5d] loss: %.3f ======' %
                       (epoch + 1, i + 1, running_loss))
-                #running_loss = 0.0
+                # running_loss = 0.0
             if i % 50 == 49:  # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 50))
                 running_loss = 0.0
 
         # middle save
-        #save_path = "/home/mll/v_mll3/OCR_data/VGG_character/model/skddn_temp_ep{}_ver{}.pth".format(epoch,version)
-        #torch.save(net.state_dict(), save_path)
+        # save_path = "/home/mll/v_mll3/OCR_data/VGG_character/model/skddn_temp_ep{}_ver{}.pth".format(epoch,version)
+        # torch.save(net.state_dict(), save_path)
     ''' original
             running_loss += loss.item()
             if i % 50 == 49:    # print every 2000 mini-batches
@@ -277,6 +289,7 @@ def train():
 
     save_path = "/home/mll/v_mll3/OCR_data/VGG_character/model/{}_.pth".format(model_name)
     torch.save(net.state_dict(), save_path)
+
 
 def validation():
     class_correct = list(0. for i in range(label_count))
@@ -291,7 +304,7 @@ def validation():
             images = images.cuda()
             labels = labels.cuda()
             outputs, _, result, vector = net(images)
-            #outputs, _ = net(images)                   #origin
+            # outputs, _ = net(images)                   #origin
             _, predicted = torch.max(outputs, 1)
             c = (predicted == labels).squeeze()
             for i in range(len(labels)):
@@ -301,38 +314,37 @@ def validation():
                 class_vector[label].append(vector)
                 class_result[label].append(result)
 
-
     # log file save
-    file = open('/home/mll/v_mll3/OCR_data/VGG_character/Skeletonize_DNN/Log/Validation_log_{}_.txt'.format(model_name),'w')
+    file = open('/home/mll/v_mll3/OCR_data/VGG_character/Skeletonize_DNN/Log/Validation_log_{}_.txt'.format(model_name),
+                'w')
 
     # vector, result save path.
-    newPath = '/home/mll/v_mll3/OCR_data/VGG_character/Skeletonize_DNN/Log/Valid_log_vector,result_{}'.format(model_name)
+    newPath = '/home/mll/v_mll3/OCR_data/VGG_character/Skeletonize_DNN/Log/Valid_log_vector,result_{}'.format(
+        model_name)
     if not (os.path.isdir(newPath)):  # 새  파일들을 저장할 디렉토리를 생성
         os.makedirs(os.path.join(newPath))
 
-
-
     file.write("Label                               correct count  |  total count \n")
-    total_count =0
-    correct_count=0
+    total_count = 0
+    correct_count = 0
     for i in range(label_count):
         print('Accuracy class_correctof %5s : %2d %%' % (
             image_datasets[VAL].classes[i], 100 * class_correct[i] / class_total[i]))
-        total_count = total_count+class_total[i]
+        total_count = total_count + class_total[i]
         correct_count = correct_count + class_correct[i]
-        file.write('Accuracy class_correctof %5s : %2d %%' % (image_datasets[VAL].classes[i], 100 * class_correct[i] / class_total[i]))
+        file.write('Accuracy class_correctof %5s : %2d %%' % (
+        image_datasets[VAL].classes[i], 100 * class_correct[i] / class_total[i]))
         file.write("    {0:<10}".format(class_correct[i]))
         file.write("| {0:<10} \n".format(class_total[i]))
 
         # vector log file save
         file2 = open('{}/Valid_vector_label[{}]_.txt'.format(newPath, image_datasets[VAL].classes[i]), 'w')
         torch.save(vector, "{}/Valid_vector_label[{}]_.pt".format(newPath, image_datasets[VAL].classes[i]))
-        file3 = open('{}/Valid_result_label[{}]_.txt'.format(newPath, image_datasets[VAL].classes[i], version, epoch_count), 'w')
+        file3 = open(
+            '{}/Valid_result_label[{}]_.txt'.format(newPath, image_datasets[VAL].classes[i], version, epoch_count), 'w')
         vector2 = vector.tolist()
 
         # result log file save
-
-
 
         for j in range(len(class_vector[i])):
             file3.write(str(class_result[i][j]))
@@ -340,21 +352,21 @@ def validation():
 
         for j in vector2:
             for k in j:
-                file2.write(str(k) +" ")
+                file2.write(str(k) + " ")
             file2.write("\n\n")
-
 
         file2.close()
         file3.close()
-        total_acc =  total_acc + (100 * class_correct[i] / class_total[i])
+        total_acc = total_acc + (100 * class_correct[i] / class_total[i])
 
-    print('Accuracy total class_correct of  : %2d %%' % ( total_acc/label_count) )
+    print('Accuracy total class_correct of  : %2d %%' % (total_acc / label_count))
     file.write('total correct : %2d | total count  %2d  \n' % (correct_count, total_count))
-    file.write('Accuracy total class_correct of  : %2d %%' % ( total_acc/label_count) )
+    file.write('Accuracy total class_correct of  : %2d %%' % (total_acc / label_count))
 
     file.close()
     file2.close()
     file3.close()
+
 
 def test():
     class_correct = list(0. for i in range(label_count))
@@ -374,7 +386,6 @@ def test():
                 label = labels[i]
                 class_correct[label] += c[i].item()
                 class_total[label] += 1
-
 
     file = open(
         '/home/mll/v_mll3/OCR_data/VGG_character/Skeletonize_DNN/Log/Test_log_{}_.txt'.format(model_name),
@@ -398,6 +409,7 @@ def test():
     file.write('total correct : %2d | total count  %2d  \n' % (correct_count, total_count))
     file.close()
 
+
 # VGG-16 Takes 224x224 images as input, so we resize all of them
 data_transforms = {
     TRAIN: transforms.Compose([
@@ -420,38 +432,49 @@ data_transforms = {
     ])
 }
 
-image_datasets = {
-    x: datasets.ImageFolder(
-        os.path.join(data_dir, x),
-        transform=data_transforms[x]
-    )
-    for x in [TRAIN, VAL, TEST]
-}
 
-dataloaders = {
-    x: torch.utils.data.DataLoader(
-        image_datasets[x], batch_size=batch,        #origin ver batch= 4 | ver1 batch=2 | ver2  batch=8  | ver batch=4
-        shuffle=True, num_workers=4
-    )
-    for x in [TRAIN, VAL, TEST]
-}
+class MNISTDataset(torch.utils.data.Dataset):
+    def __init__(self, file_path):
+        self.file = file_path
+        self.data = pd.read_csv(file_path, dtype=np.float32)
+        self.label = self.data.label.values
+        self.len = len(self.label)
+        self.data = self.data.values[:, 1:] / 255
+
+    def __getitem__(self, index):
+        return self.data[index], self.label[index]
+
+    def __len__(self):
+        return self.len
 
 
-dataset_sizes = {x: len(image_datasets[x]) for x in [TRAIN, VAL, TEST]}
+train_csv = '/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/Train/emnist-byclass-train.csv'
+test_csv = '/home/mll/v_mll3/OCR_data/dataset/MNIST_dataset/EMNIST_byclass/Test/emnist-byclass-test.csv'
 
-for x in [TRAIN, VAL, TEST]:
-    print("Loaded {} images under {}".format(dataset_sizes[x], x))
+train_dataloader = torch.utils.data.DataLoader(
+    MNISTDataset(train_csv), batch_size=batch, shuffle=True, num_workers=4
+)
 
+test_dataloader = torch.utils.data.DataLoader(
+    MNISTDataset(test_csv), batch_size=batch, shuffle=True, num_workers=4
+)
+
+#csv_dataset_sizes = {x: len(train_dataloader[x]) for x in [TRAIN,TEST]}
+
+
+print("Loaded {} images under {}".format(train_dataloader,train_csv))
+print("Loaded {} images under {}".format(test_dataloader,test_csv))
 print("Classes: ")
-class_names = image_datasets[TRAIN].classes
-label_count = len(image_datasets[TRAIN].classes)
-print(image_datasets[TRAIN].classes)
+class_names = train_dataloader.classes
+label_count = len(train_dataloader.classes)
+print(train_dataloader.classes)
+
 
 s = 't'
-while(s != "1" or s !="2"):
+while (s != "1" or s != "2"):
     print("Please command \n [ ( 1 ) train the model |  ( 2 ) load pre-trained model | (3) test_sample_case ] ")
     s = input()
-    if s== "1":
+    if s == "1":
 
         # need to set epoch, version info.
 
@@ -463,7 +486,7 @@ while(s != "1" or s !="2"):
         break
 
 
-    elif s=="2":
+    elif s == "2":
         net = Net()
         model_dir, model_name = model_loader()
         net.load_state_dict(torch.load(model_dir))
@@ -481,7 +504,7 @@ while(s != "1" or s !="2"):
         break
 
 
-    elif s=="3":
+    elif s == "3":
         net = Net()
         model_dir, model_name = model_loader()
         net.load_state_dict(torch.load(model_dir))
@@ -497,17 +520,5 @@ while(s != "1" or s !="2"):
     else:
         print("check command")
 
-
-
 print("-----------------------")
 print("Done.")
-
-
-'''
-
-'''
-
-
-# functions to show an image
-
-print("Done!")
